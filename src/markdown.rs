9bf62ddc9a71158b7d152010f760f777071d08d0
olivia <olivia@fastmail.com>
2018-06-20 22:51:45 +0200

    use comrak imports

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,4 +1,4 @@
-use comrak;
+use comrak::{markdown_to_html, ComrakOptions};
 use nya::{create_middleware, MiddlewareFunction, SimpleFile};
 use util;
 
@@ -6,10 +6,7 @@ pub fn middleware() -> MiddlewareFunction {
     create_middleware(|files: &mut Vec<SimpleFile>| {
         for file in files {
             if util::ext_matches(file, ".md") {
-                file.content = comrak::markdown_to_html(
-                    file.content.as_str(),
-                    &comrak::ComrakOptions::default(),
-                );
+                file.content = markdown_to_html(file.content.as_str(), &ComrakOptions::default());
                 util::rename_ext(file, "html");
             }
         }
