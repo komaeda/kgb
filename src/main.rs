cfe3e742f850d5c8e8b78796986e2c2343228e99
olivia <olivia@fastmail.com>
2018-07-18 13:26:18 +0200

    0.0.4

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use std::path::PathBuf;
 
 fn main() {
     let matches = App::new("kg")
-        .version("0.0.3")
+        .version("0.0.4")
         .author("Olivia Hugger <olivia@fastmail.com>")
         .about("A static site generator")
         .arg(
